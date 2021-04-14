9531d3eed17cdde97ad5a88426dcf05f1448912e
belkin <sahilnair71@gmail.com>
2021-04-15 00:38:29 +0530

    environmet.prod set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDlWYtT7lzu3wOtw_w1qCmvPnIxCe6kRGA",
+    authDomain: "ferw-3fe2f.firebaseapp.com",
+    projectId: "ferw-3fe2f",
+    storageBucket: "ferw-3fe2f.appspot.com",
+    messagingSenderId: "586292820873",
+    appId: "1:586292820873:web:bb666b133c919703e4bf58",
+    measurementId: "G-Y5WP219WNR",
+  },
 };
